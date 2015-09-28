4360eef957a1be25c83d16d170d27d30f77d30ba
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2015-09-27 21:22:14 -0700

    Remove the old, sorta-owning <ring_view.h> in favor of the assignment-based one.

--- ring_view.h
+++ ring_view.h
@@ -5,57 +5,70 @@
 //
 
 // Goal: A non-owning ring-buffer class.
-// It should be empty-able, i.e., it should construct and destroy its elements.
-// Since no objects are ever moved, it should support non-copy/moveable objects.
-//
-// However, for types that DO support assignment, it should be efficient to "overwrite";
-// the operation of pop-and-then-push(v) should be POSSIBLE to achieve as a
-// single assignment, if the user explicitly requests it.
+// Since it's non-owning, it's not allowed to construct or destroy elements
+// of the underlying container. Push and pop are implemented as assignment and
+// bookkeeping, respectively.
 //
 
 #include <cassert>
 #include <cstddef>
 #include <iterator>
+#include <type_traits>
 #include <utility>
 
 // detail
 template<class, bool> class ring_view_iterator;
 
 template<class T>
+struct default_popper {
+    void operator()(T&) { }  // do nothing, return void
+};
+
+// Another useful popper class would be this one, which moves-from the
+// popped element. This lets us make a ring-buffer of shared_ptr or
+// unique_ptr with the appropriate semantics.
+//
+template<class T>
+struct move_popper {
+    T operator()(T& t) { return std::move(t); }
+};
+
+template<class T, class Popper = default_popper<T>>
 class ring_view
 {
 public:
+    using value_type = T;
     using pointer = T*;
     using reference = T&;
     using const_reference = const T&;
     using size_type = std::size_t;
-    using iterator = ring_view_iterator<T, false>;
-    using const_iterator = ring_view_iterator<T, true>;
-
-    // Construct an empty ring_view.
-    //
-    ring_view(void *data, size_type capacity) noexcept :
-        data_(static_cast<T*>(data)),
-        empty_(true),
-        capacity_(capacity),
-        front_idx_(0),
-        back_idx_(0)
-    {}
+    using iterator = ring_view_iterator<ring_view, false>;
+    using const_iterator = ring_view_iterator<ring_view, true>;
 
     // Construct a full ring_view.
     //
     template<class ContiguousIterator>
-    ring_view(ContiguousIterator begin, ContiguousIterator end) noexcept :
+    ring_view(ContiguousIterator begin, ContiguousIterator end, Popper p = Popper()) noexcept :
         data_(&*begin),
-        empty_(false),
+        size_(end - begin),
         capacity_(end - begin),
         front_idx_(0),
-        back_idx_(0)
+        popper_(std::move(p))
     {}
 
-    // Notice that an iterator contains a pointer to the ring_view
-    // itself. Destroying ring_view rv invalidates rv.begin(),
-    // just as with an owning container.
+    // Construct a "partially full" ring_view.
+    //
+    template<class ContiguousIterator>
+    ring_view(ContiguousIterator begin, ContiguousIterator end, ContiguousIterator first, size_type size, Popper p = Popper()) noexcept :
+        data_(&*begin),
+        size_(size),
+        capacity_(end - begin),
+        front_idx_(first - begin),
+        popper_(std::move(p))
+    {}
+
+    // Notice that an iterator contains a pointer to the ring_view itself.
+    // Destroying ring_view rv invalidates rv.begin(), just as with an owning container.
     //
     iterator begin() noexcept { return iterator(0, this); }
     iterator end() noexcept { return iterator(size(), this); }
@@ -67,108 +80,77 @@ public:
     const_reference front() const noexcept { return *begin(); }
     const_reference back() const noexcept { return *(end() - 1); }
 
-    bool empty() const noexcept { return empty_; }
-    bool full() const noexcept { return (not empty_) && (front_idx_ == back_idx_); }
-    size_type size() const noexcept { return full() ? capacity_ : (back_idx_ - front_idx_ + capacity_) % capacity_; }
+    bool empty() const noexcept { return size_ == 0; }
+    bool full() const noexcept { return size_ == capacity_; }
+    size_type size() const noexcept { return size_; }
     size_type capacity() const noexcept { return capacity_; }
 
-    // try_emplace_back() constructs a new element at the back of the ring.
-    // If the ring is full, we return false and don't do anything else.
-    //
-    template<class... Args>
-    bool try_emplace_back(Args&&... args)
-    {
-        if (full()) {
-            return false;
-        }
-        emplace_back(std::forward<Args>(args)...);
-        return true;
-    }
-
-    // Calling emplace_back() on a full ring is undefined,
-    // in the same way as calling pop_front() on any empty container is undefined.
-    // Without emplace_back(), you can't use ring_view as a std::queue.
-    //
-    template<class... Args>
-    void emplace_back(Args&&... args)
-    {
-        assert(!full());
-        new ((void*)&data_[back_idx_]) T(std::forward<Args>(args)...);
-        back_idx_ = (back_idx_ + 1) % capacity_;
-        empty_ = false;
-    }
-
-    // Calling push_back() on a full ring is undefined,
-    // in the same way as calling pop_front() on any empty container is undefined.
-    // Without push_back(), you can't use ring_view as a std::queue.
-    //
-    void push_back(const T& value) { emplace_back(value); }
-    void push_back(T&& value) { emplace_back(std::move(value)); }
-
-    // pop_front() destroys the element at the front of the ring.
+    // pop_front() increments the index of the begin of the ring.
+    // Notice that it does not destroy anything.
     // Calling pop_front() on an empty ring is undefined,
     // in the same way as calling pop_front() on an empty vector or list is undefined.
     // Without pop_front(), you can't use ring_view as a std::queue.
     //
-    bool pop_front()
+    auto pop_front()
     {
         assert(!empty());
-        data_[front_idx_].~T();
+        auto old_front_idx = front_idx_;
         front_idx_ = (front_idx_ + 1) % capacity_;
-        if (front_idx_ == back_idx_) {
-            empty_ = true;
+        --size_;
+        return popper_(data_[old_front_idx]);
+    }
+
+    // push_back() assigns a new value to the element
+    // at the end of the ring, and makes that element the
+    // new back of the ring. If the ring is full before
+    // the call to push_back(), we rotate the indices and
+    // invalidate all iterators into the ring.
+    // Without push_back(), you can't use ring_view as a std::queue.
+    //
+    template<bool b=true, typename=std::enable_if_t<b && std::is_copy_assignable<T>::value>>
+    void push_back(const T& value) noexcept(std::is_nothrow_copy_assignable<T>::value)
+    {
+        data_[back_idx()] = value;
+        if (not full()) {
+            ++size_;
         }
-        return true;
     }
 
-    // assign_when_full() assigns a new value to the element
-    // at the front of the ring, and makes that element the
-    // new back of the ring by rotating the indices.
-    // It invalidates all iterators into the ring.
-    // If the ring is NOT full, undefined behavior.
-    //
-    void assign_when_full(const T& value)  // and another version for T&&
+    template<bool b=true, typename=std::enable_if_t<b && std::is_move_assignable<T>::value>>
+    auto push_back(T&& value) noexcept(std::is_nothrow_move_assignable<T>::value)
     {
-        assert(full());
-        data_[front_idx_] = value;
-        back_idx_ = front_idx_ = (front_idx_ + 1) % capacity_;
-    }
-
-    // Should this be named push_back?
-    // If the ring is full, this function invalidates all iterators into the ring.
-    //
-    template<class... Args>
-    void emplace_or_assign(Args&&... args)
-    {
-        if (not try_emplace_back(std::forward<Args>(args)...)) {
-            assign_when_full(T(std::forward<Args>(args)...));
+        data_[back_idx()] = std::move(value);
+        if (not full()) {
+            ++size_;
         }
     }
 
 private:
-    friend class ring_view_iterator<T, true>;
-    friend class ring_view_iterator<T, false>;
+    friend class ring_view_iterator<ring_view, true>;
+    friend class ring_view_iterator<ring_view, false>;
 
     // Should this member function be public?
     //
     reference at(size_type i) noexcept { return data_[(front_idx_ + i) % capacity_]; }
     const_reference at(size_type i) const noexcept { return data_[(front_idx_ + i) % capacity_]; }
 
+    size_type back_idx() const noexcept { return (front_idx_ + size_) % capacity_; }
+
     T *data_;
-    bool empty_;
+    size_type size_;
     size_type capacity_;
     size_type front_idx_;
-    size_type back_idx_;
+    Popper popper_;
 };
 
-template<class T, bool is_const>
+template<class RV, bool is_const>
 class ring_view_iterator
 {
 public:
-    using value_type = T;
+    using value_type = typename RV::value_type;
     using difference_type = std::ptrdiff_t;
-    using pointer = typename std::conditional<is_const, const T, T>::type*;
-    using reference = typename std::conditional<is_const, const T, T>::type&;
+    using pointer = typename std::conditional_t<is_const, const value_type, value_type>*;
+    using reference = typename std::conditional_t<is_const, const value_type, value_type>&;
     using iterator_category = std::random_access_iterator_tag;
 
     ring_view_iterator() = default;
@@ -179,20 +161,25 @@ public:
     ring_view_iterator& operator--() noexcept { ++idx_; return *this; }
     ring_view_iterator operator--(int) noexcept { auto r(*this); ++*this; return r; }
 
-    template<bool C> bool operator==(const ring_view_iterator<T,C>& rhs) const noexcept { return idx_ == rhs.idx_; }
-    template<bool C> bool operator!=(const ring_view_iterator<T,C>& rhs) const noexcept { return idx_ != rhs.idx_; }
-    template<bool C> bool operator<(const ring_view_iterator<T,C>& rhs) const noexcept { return idx_ < rhs.idx_; }
-    template<bool C> bool operator<=(const ring_view_iterator<T,C>& rhs) const noexcept { return idx_ <= rhs.idx_; }
-    template<bool C> bool operator>(const ring_view_iterator<T,C>& rhs) const noexcept { return idx_ > rhs.idx_; }
-    template<bool C> bool operator>=(const ring_view_iterator<T,C>& rhs) const noexcept { return idx_ >= rhs.idx_; }
+    friend ring_view_iterator& operator+=(ring_view_iterator& it, int i) noexcept { it.idx_ += i; return it; }
+    friend ring_view_iterator& operator-=(ring_view_iterator& it, int i) noexcept { it.idx_ -= i; return it; }
+    friend ring_view_iterator operator+(ring_view_iterator it, int i) noexcept { it += i; return it; }
+    friend ring_view_iterator operator-(ring_view_iterator it, int i) noexcept { it -= i; return it; }
+
+    template<bool C> bool operator==(const ring_view_iterator<RV,C>& rhs) const noexcept { return idx_ == rhs.idx_; }
+    template<bool C> bool operator!=(const ring_view_iterator<RV,C>& rhs) const noexcept { return idx_ != rhs.idx_; }
+    template<bool C> bool operator<(const ring_view_iterator<RV,C>& rhs) const noexcept { return idx_ < rhs.idx_; }
+    template<bool C> bool operator<=(const ring_view_iterator<RV,C>& rhs) const noexcept { return idx_ <= rhs.idx_; }
+    template<bool C> bool operator>(const ring_view_iterator<RV,C>& rhs) const noexcept { return idx_ > rhs.idx_; }
+    template<bool C> bool operator>=(const ring_view_iterator<RV,C>& rhs) const noexcept { return idx_ >= rhs.idx_; }
 
 private:
-    friend class ring_view<T>;
-    using size_type = typename ring_view<T>::size_type;
+    friend RV;
 
-    ring_view_iterator(size_type idx, ring_view<T> *rv) noexcept : idx_(idx), rv_(rv) {}
+    using size_type = typename RV::size_type;
+
+    ring_view_iterator(size_type idx, RV *rv) noexcept : idx_(idx), rv_(rv) {}
 
     size_type idx_;
-    ring_view<T> *rv_;
+    RV *rv_;
 };
-
--- ring_view.test.cc
+++ ring_view.test.cc
@@ -1,106 +1,29 @@
+#include "assignment_ring_view.h"
 
-#include "ring_view.h"
-
-#include <array>
-#include <cassert>
 #include <memory>
+#include <queue>
+#include <vector>
 
-// An example of how to use the ring_view as a "history".
-void add_to_history(ring_view<int> rv, int new_value)
-{
-    if (not rv.try_emplace_back(new_value)) {
-        rv.assign_when_full(new_value);
-    }
-}
-
-// An example of how to use the ring_view as a concurrent_bounded_queue. Ish.
-template<class T>
-struct unique_bounded_queue {
-    using raw_T = typename std::aligned_storage<sizeof(T), alignof(T)>::type;
-
-    unique_bounded_queue(size_t cap) :
-        data_(std::get_temporary_buffer<T>(cap).first, get_deleter()),  // no error-checking, mind you
-        rv_(data_.get(), cap)
-    {
-        assert(rv_.capacity() == cap);
-        assert(rv_.size() == 0);
-    }
-
-    void push(const T& value) { rv_.emplace_or_assign(value); }
-    void push(T&& value) { rv_.emplace_or_assign(value); }
-    void pop() { rv_.pop_front(); }
-    auto begin() { return rv_.begin(); }
-    auto end() { return rv_.end(); }
-
-private:
-    static auto get_deleter() { return [](T *p){ std::return_temporary_buffer(p); }; }
-
-    std::unique_ptr<T, decltype(get_deleter())> data_;
-    ring_view<T> rv_;
-};
+using T = std::unique_ptr<int>;
+using RVT = ring_view<T, move_popper<T>>;
 
 int main()
 {
-    unique_bounded_queue<int> ubq(4);
-    for (int v : {1,2,3,4,5,6,7,8}) {
-        ubq.push(v);
-        for (int i : ubq) {
-            printf(" %d", i);
-        }
-        printf("\n");
-    }
+    std::vector<T> vec(100);
+    RVT rv(vec.begin(), vec.end());
+    assert(rv.capacity() == 100);
+    assert(rv.size() == 100);
+    rv.push_back(nullptr);
+    assert(rv.capacity() == 100);
+    assert(rv.size() == 100);
+    rv.pop_front();
+    assert(rv.capacity() == 100);
+    assert(rv.size() == 99);
 
-    // Test "observing" generic bytes as if they were `T`...
-    {
-        // Keep track of ctor and dtor calls.
-        int ctor_count = 0;
-        int dtor_count = 0;
-
-        // Example item that increments the above variables on ctor/dtor.
-        struct example_item
-        {
-            int& ctor_count_ref;
-            int& dtor_count_ref;
-
-            example_item(int& ccr, int& dcr) : ctor_count_ref(ccr), dtor_count_ref(dcr) { ++ctor_count_ref; }
-            ~example_item() { ++dtor_count_ref; }
-        };
-
-        // Create some bytes.
-        using example_raw_storage = std::aligned_storage_t<sizeof(example_item), alignof(example_item)>;
-        using example_item_raw_array = std::array<example_raw_storage, 32>;
-        example_item_raw_array eira;
-
-        // Look at those bytes as if they were `example_item` instances.
-        ring_view<example_item> rw(eira.data(), 8);
-
-        assert(rw.empty());
-        assert(rw.size() == 0);
-        assert(rw.capacity() == 8);
-
-        assert(ctor_count == 0);
-        assert(dtor_count == 0);
-
-        rw.emplace_back(ctor_count, dtor_count);
-        rw.emplace_back(ctor_count, dtor_count);
-        rw.emplace_back(ctor_count, dtor_count);
-
-        assert(!rw.empty());
-        assert(rw.size() == 3);
-        assert(rw.capacity() == 8);
-
-        assert(ctor_count == 3);
-        assert(dtor_count == 0);
-
-        rw.pop_front();
-        rw.pop_front();
-        rw.pop_front();
-
-        assert(rw.empty());
-        assert(rw.size() == 0);
-        assert(rw.capacity() == 8);
-
-        assert(ctor_count == 3);
-        assert(dtor_count == 3);
-    }
+    std::queue<T, RVT> q(rv);
+    q.push(nullptr);
+    q.push(nullptr);
+    q.front();
+    q.back();
+    q.pop();
 }
